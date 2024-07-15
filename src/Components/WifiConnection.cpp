1755f8f98b81a39519b79679aa5ceace0cec2473
Boris Brock <gabrielborisbrock@gmail.com>
2024-07-15 16:37:26 +0200

    Migrating to WifiManager

--- src/Components/WifiConnection.cpp
+++ src/Components/WifiConnection.cpp
@@ -1,42 +1,14 @@
 #include <Arduino.h>
-#include <WiFi.h>
-#include "../Configuration/Credentials.h"
+#include <WiFiManager.h>
 #include "WifiConnection.h"
 
-void WiFiStationConnected(WiFiEvent_t event, WiFiEventInfo_t info)
-{
-    Serial.println("Connected to AP successfully!");
-}
-
-void WiFiGotIP(WiFiEvent_t event, WiFiEventInfo_t info)
-{
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-}
-
-void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info)
-{
-    Serial.println("Disconnected from WiFi access point");
-    Serial.print("WiFi lost connection. Reason: ");
-    Serial.println(info.wifi_sta_disconnected.reason);
-    Serial.println("Trying to Reconnect");
-    WiFi.begin(ssid, password);
-}
+WiFiManager wifiManager;
 
 void WifiConnection::Init()
 {
-    // Delete old config
-    Serial.println("Preparing Wifi");
-    WiFi.disconnect(true);
-    delay(100);
+    wifiManager.setConfigPortalTimeout(180);
+    wifiManager.setTitle("HeidelBridge");
+    wifiManager.setShowInfoUpdate(false);
 
-    // Register WiFi events
-    WiFi.onEvent(WiFiStationConnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_CONNECTED);
-    WiFi.onEvent(WiFiGotIP, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_GOT_IP);
-    WiFi.onEvent(WiFiStationDisconnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
-
-    // Start Wifi connection
-    Serial.printf("[Connecting to WiFi network '%s'...\n", ssid);
-    WiFi.begin(ssid, password);
+    wifiManager.autoConnect("HeidelBridge", "heidelbridge");
 }

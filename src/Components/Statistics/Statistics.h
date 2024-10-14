05e66d59c14131fafc3d880364a468543ca834b9
Boris Brock <gabrielborisbrock@gmail.com>
2024-10-14 20:16:56 +0200

    Increasing uptime counter to 64 bits

--- src/Components/Statistics/Statistics.h
+++ src/Components/Statistics/Statistics.h
@@ -6,7 +6,7 @@ struct Statistics
     uint32_t NumModbusWriteErrors = 0;
     uint32_t NumWifiDisconnects = 0;
     uint32_t NumMqttDisconnects = 0;
-    uint32_t UptimeS = 0;
+    uint64_t UptimeS = 0;
 };
 
 // Global statistics instance

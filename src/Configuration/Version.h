50e05768a66a890827c1af10bfcea0badba707d1
Boris Brock <gabrielborisbrock@gmail.com>
2025-06-25 21:57:59 +0200

    Version bump to 3.2.0

--- src/Configuration/Version.h
+++ src/Configuration/Version.h
@@ -3,6 +3,6 @@
 namespace Version
 {
     constexpr uint8_t Major = 3;
-    constexpr uint8_t Minor = 1;
-    constexpr uint8_t Patch = 2;
+    constexpr uint8_t Minor = 2;
+    constexpr uint8_t Patch = 0;
 }
